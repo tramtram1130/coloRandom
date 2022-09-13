0c20009e0e59c4a02ddd8599077d6ce54b8cac31
Spencer Haka <spencer.haka@gmail.com>
2022-09-12 20:50:44 -0600

    Add method to pallete class to hold 5 random colors on each instantiation

--- colors.js
+++ colors.js
@@ -8,13 +8,11 @@ class Colors {
     var characters = ['A', 'B', 'C', 'D', 'E', 'F', 0 , 1, 2, 3, 4, 5, 6, 7, 8, 9];
     var hexCode = '';
     for (var i = 0; i < 6; i++) {
-      var randomNum = Math.floor(Math.random() * 16 + 1);
+      var randomNum = Math.floor(Math.random() * 16);
       hexCode += characters[randomNum];
     }
     return hexCode;
   }
 }
 
-var color = new Colors;
-
-console.log(color);
+module.exports = Colors;
--- palette.js
+++ palette.js
@@ -1,6 +1,21 @@
+const Colors = require('./colors.js');
+
 class Palette {
-  constructor(colors) {
-    this.colors = [colors, colors, colors, colors, colors]
+  constructor() {
+    this.colors = this.newPalette();
     this.id = Date.now()
   }
+
+  newPalette() {
+    var palette = [];
+    for (var i = 0; i < 5; i++) {
+      palette.push(new Colors);
+    }
+    return palette;
+  }
 }
+
+var newPalette = new Palette;
+
+console.log(newPalette);
+
